915b5db41881f4f2bdca06151bfea3c166eb7e34
Vasilis CodeX <vasilis.tris@gmail.com>
2020-12-23 13:05:13 +0200

    bind country routes to main server

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const Express = require('./express');
 const Router = require('./routing/router');
+const CountryRoutesBuilder = require('./routing/routes/countryRoutes');
 
 const express = new Express(
     new Router([
+        new CountryRoutesBuilder()
     ]),
 );
 

f0375f17d70112b5cbc826b62b2fa9aa16d2b377
Vasilis CodeX <vasilis.tris@gmail.com>
2020-12-23 12:45:42 +0200

    add route scripts

---
+++ routing/countryRoutes.js
@@ -0,0 +1,15 @@
+const CountryController = require('../../controllers/countryController');
+const RoutesBase = require('./routesBase');
+
+class ContryRoutes extends RoutesBase {
+    constructor() {
+        super( CountryController );
+    }
+
+    getRoutes() {
+        this.addRoute('/countries', 'get', 'getCountries');
+        return this.routes;
+    }
+}
+
+module.exports = ContryRoutes;
